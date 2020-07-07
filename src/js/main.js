5606b0f5b7349e7b3603adea8bca50461fe97e9b
uzinok <uzinok@yandex.ru>
2020-07-07 18:37:09 +0400

    без футера предварительно готово

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,22 @@
 window.onload = function () {
+    // слайдеры
     slider(document.querySelector('.slider-1-js'), 2);
     slider(document.querySelector('.slider-2-js'), 1);
-    
-    document.querySelector('.btn').addEventListener('click', function () {
-        // вызываем функцию popup
-        popup(document.querySelector('.popap'));
-      })
 
-    document.querySelector('.header').addEventListener('click', function() {
-        console.log(document.querySelector('.slider-1-js button[data-slide="2"]'))
-    })
+    // popup
+    if (document.querySelector('.btn-popup')) {
+        let btnPopup = document.querySelectorAll('.btn-popup');
+
+        for (let i = 0; i < btnPopup.length; i++) {
+            console.log(btnPopup)
+            
+            btnPopup[i].addEventListener('click', function () {
+                // вызываем функцию popup
+                popup(document.querySelector('.popap'));
+            })
+        }
+    }
+
+
+
 };
